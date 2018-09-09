082c9d7e86184be6c7241ce1fbb23dee2c3c755c
leoScorpio <leo15615452090@163.com>
2018-09-10 07:40:39 +0800

    yulu

---
+++ src/main/java/src/com/pddata/PdserviceApplication.java
@@ -0,0 +1,12 @@
+package src.com.pddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdserviceApplication.class, args);
+	}
+}
